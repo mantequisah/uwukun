e06dfde497c6d4cfc2d7be847ca80b2efc8c0841
carlos Garzon <uwuitofdz@gmail.com>
2022-03-25 10:31:07 -0500

    danielkun

---
+++ uwu1.py
@@ -0,0 +1,18 @@
+mango=int(2500)
+fresa=int(500)
+piñita=int(4000)
+mamoncillo	=int(6000)
+
+frutaseleccionada=input("\n elige una fruta entre mangoi, piña,fresa, mamoncillo")
+
+if frutaseleccionada	== mango:
+	print(mango)
+	else:
+	if frutaseleccionada	==fresa:
+		print(fresa)
+		else:
+			if frutaseleccionada==piña:
+				print(piñita)
+			else:
+				if frutaseleccionada	==mamoncillo:
+					print(mamoncillo)
